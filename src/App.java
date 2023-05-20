475a522dd014db89cf8caa18526a596e65230fa7
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2023-05-19 23:30:09 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // era dessa forma no ja 7
+        // new Thread(New Runnable(){
+        // @Override
+        // public void run() {
+        // System.out.println("Ola Mundo");
+        // }).run();
+
+        // com as atualizaçoẽs do java 8 ficou dessa Formato
+        
+        // com a nova atualização o java faz a implentação do metodo rum() da interface
+        // Runnable de forma automatica devido ser o unico metodo abstrato que e
+        // encontrado na interface seguindo o padrão SAM (single abstract method)
+
+        new Thread(() -> System.out.println("ola Mundo")).run();
+    }
+}
---
+++ src/Stream.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Stream {
+    public static void main(String[] args) {
+          // tipo   referenceArrays  
+        List<Integer> asList = Arrays.asList(1,2,3,4);
+        
+        System.out.println("\ntodos os numeros do Arrays com for");
+            //tipo  reference  OndePega   
+        for (Integer integer : asList) {
+            System.out.println(integer);
+        }
+
+        // stream - Fluxo de dados 
+        
+        System.out.println("\ntodos os numeros do Arrays com stream()");
+        asList.stream()
+              .forEach(e-> System.out.println(e));
+
+        System.out.println("\nnumeros pares com stream()");
+        asList.stream()
+            .filter(e -> e % 2 ==0)
+            .forEach(e-> System.out.println(e));
+    }
+}
